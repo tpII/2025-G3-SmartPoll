d3d18167cf2e31f4dce58df7cc9ee3e98fb78378
Gonzalo Blasco <gonzagabino@gmail.com>
2025-09-13 11:38:01 -0300

    feat: Add initial project structure for backend

---
+++ backend/src/test/java/smartpoll/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package smartpoll.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
