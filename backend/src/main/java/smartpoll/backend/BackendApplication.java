73d41670c68aee93a5ee721e7f7feca1474be628
Gonzalo Blasco <gonzagabino@gmail.com>
2025-09-13 12:09:05 -0300

    feat: Add Spring Boot DevTools dependency and create a simple index endpoint

--- backend/src/main/java/smartpoll/backend/BackendApplication.java
+++ backend/src/main/java/smartpoll/backend/BackendApplication.java
@@ -2,7 +2,10 @@ package smartpoll.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BackendApplication {
 
@@ -10,4 +13,8 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "New Hello World!";
+    }
 }
