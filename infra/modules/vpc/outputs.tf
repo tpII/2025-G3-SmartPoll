64ee62e4a0d3abd20e71ff421d624777d442edd8
ramiro <ramiro.cabral@alu.ing.unlp.edu.ar>
2025-09-27 12:39:14 -0300

    fix: moved security groups definitions

--- infra/modules/vpc/outputs.tf
+++ infra/modules/vpc/outputs.tf
@@ -10,10 +10,6 @@ output "private_subnets_id" {
   value = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
 }
 
-output "security_groups_id" {
-  value = [aws_security_group.allow_http.id]
-}
-
 output "public_route_table_id" {
   value = aws_route_table.public_rt.id
 }
