03ed192bd756a5d61253de592c8f37c4633a2314
ramiro <ramiro.cabral@alu.ing.unlp.edu.ar>
2025-11-19 22:12:15 -0300

    added cloudwatch logs

--- infra/modules/elb/main.tf
+++ infra/modules/elb/main.tf
@@ -14,7 +14,7 @@ resource "aws_lb_target_group" "web_tg" {
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
-    unhealthy_threshold = 2
+    unhealthy_threshold = 3
   }
 }
 
